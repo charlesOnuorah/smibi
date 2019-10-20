46732575fd46fa2f058c7112431ca5ab7215d0f2
iamNarcisse <mechikaegon@gmail.com>
2019-10-20 15:49:59 +0100

    got it right this time

--- src/components/pages/chatbot/Chatbot.js
+++ src/components/pages/chatbot/Chatbot.js
@@ -3,14 +3,26 @@ import { UserReplyCard, BotReplyCard } from "../../simbicards/ReplyCard";
 import ReactTextareaAutocomplete from "@webscopeio/react-textarea-autocomplete";
 import { Picker, emojiIndex } from "emoji-mart";
 import { Smile } from "react-feather";
+import { TextAreaComponent } from "./Textarea";
 
 // import "./chatbot.css";
 
 export default class ChatBotComponent extends Component {
   state = {
     chat: false,
-    showEmoji: false
+    showEmoji: false,
+    user_id: 1,
+    showMessage: false,
+    message: "",
+    messageArr: [
+      {
+        data_id: 1,
+        // sender_id: 1,
+        message: "Hi, I am Simbi Bot"
+      }
+    ]
   };
+
   chat = () => {
     this.setState(previousState => ({
       chat: !previousState.chat
@@ -24,54 +36,70 @@ export default class ChatBotComponent extends Component {
   };
 
   render() {
-    const { showEmoji } = this.state;
+    const { showEmoji, message, messageArr } = this.state;
     return (
       <Fragment>
         <div className="chat-container">
-          <div className="bot-reply-section">
+          {/* <div className="bot-reply-section">
             <BotReplyCard
               text={
                 "For God so loved the world that he gave his only begotten son"
               }
             />
-          </div>
-          <UserReplyCard text="Ye" />
+          </div> */}
+          {this.state.messageArr && (
+            <React.Fragment>
+              {this.state.messageArr.map((item, index) => {
+                return (
+                  <React.Fragment key={index}>
+                    {this.state.user_id === item.sender_id ? (
+                      <div className="user-reply-section">
+                        <UserReplyCard text={item.message} />
+                      </div>
+                    ) : (
+                      <div className="bot-reply-section">
+                        <BotReplyCard text={item.message} />
+                      </div>
+                    )}
+                  </React.Fragment>
+                );
+              })}
+            </React.Fragment>
+          )}
 
-          <div className="bot-reply-section">
-            <BotReplyCard
-              text={
-                "For God so loved the world that he gave his only begotten son"
-              }
-            />
-          </div>
-
-          <UserReplyCard text="Ye" />
-
-          <div>
-            <button onClick={() => this.showEmoji()}>
-              <Smile />
-            </button>
-
-            {showEmoji && <Picker />}
-            <ReactTextareaAutocomplete
-              loadingComponent={() => <span>Loading</span>}
-              trigger={{
-                ":": {
-                  dataProvider: token =>
-                    emojiIndex.search(token).map(o => ({
-                      colons: o.colons,
-                      native: o.native
-                    })),
-                  component: ({ entity: { native, colons } }) => (
-                    <div>{`${colons} ${native}`}</div>
-                  ),
-                  output: item => `${item.native}`
-                }
-              }}
-            />
+          <div className="textarea-container">
+            <TextAreaComponent receivemessage={this._ReceiveMessage} />
           </div>
         </div>
       </Fragment>
     );
   }
+
+  _ReceiveMessage = (message, showMessage) => {
+    const { messageArr } = this.state;
+
+    const newMessage = [...messageArr, { sender_id: 1, message: message }];
+    this.setState({
+      message,
+      showMessage,
+      messageArr: newMessage
+    });
+
+    setTimeout(() => {
+      this.botReply();
+    }, 3000);
+  };
+
+  botReply = () => {
+    const { messageArr } = this.state;
+
+    const newMessage = [
+      ...messageArr,
+      { bot: 1, message: "Hello how are you" }
+    ];
+
+    this.setState({
+      messageArr: newMessage
+    });
+  };
 }
---
+++ src/components/pages/chatbot/Textarea.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./chatbot.css";
+export class TextAreaComponent extends React.Component {
+  state = {
+    message: ""
+  };
+  render() {
+    const { message } = this.props;
+    return (
+      <React.Fragment>
+        <span>
+          <textarea
+            placeholder="Enter your message..."
+            className="text-area"
+            value={message}
+            name="message"
+            onChange={this.onSendMessage}
+          ></textarea>
+          <span className="send-chat" onClick={() => this.sendMessageHandler()}>
+            Send
+          </span>
+        </span>
+      </React.Fragment>
+    );
+  }
+
+  sendMessageHandler = () => {
+    this.props.receivemessage(this.state.message, true);
+    // this.setState({
+    //   message: ""
+    // });
+  };
+
+  onSendMessage = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value
+    });
+  };
+}
